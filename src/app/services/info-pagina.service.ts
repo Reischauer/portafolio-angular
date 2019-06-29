9481973d196d65186fb04eb9da7ede63641c5490
German Pinilla <germanalpg@hotmail.com>
2019-06-29 12:19:57 -0500

    listo para produccion

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+  equipo: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+    console.log('Servicio listo')
+    this.cargarInfo();
+    this.cargarEquipo();
+   
+  }
+
+  private cargarInfo(){
+     //Leer el archivo JSON
+     this.http.get('assets/data/data-pagina.json')
+     .subscribe((resp: InfoPagina) =>{
+
+       this.cargada = true;
+       this.info = resp;
+      // console.log(resp); 
+       //console.log(resp['facebook']); 
+     });
+  }
+
+  private cargarEquipo(){
+      //Leer el archivo JSON
+      this.http.get('https://angular-html-5fbdb.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) =>{
+
+        this.equipo = resp;
+        console.log(resp); 
+        //console.log(resp['facebook']); 
+      });
+
+  }
+}
